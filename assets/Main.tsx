dfcb543cf43db619eca175eef7dd92ba16caa9c9
MarkKoz <KozlovMark@gmail.com>
2018-10-22 20:38:16 -0700

    Add map collision
    * Move shrubbery layer behind land
    * Fix incorrect tile on a corner

--- assets/Main.tsx
+++ assets/Main.tsx
@@ -1,4 +1,179 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.1" tiledversion="2018.09.12" name="Main" tilewidth="32" tileheight="32" tilecount="232" columns="29">
  <image source="textures/tileset.png" width="950" height="256"/>
+ <tile id="39">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-cliffside.tx" x="8" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-cliffside.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-cliffside.tx" x="8" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-cliffside.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-cliffside.tx" x="8" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-cliffside.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="154">
+  <objectgroup draworder="index">
+   <object id="2" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="155">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="156">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="157">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-diagonal.tx" x="0" y="32" rotation="90"/>
+  </objectgroup>
+ </tile>
+ <tile id="160">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-diagonal.tx" x="32" y="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="163">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-diagonal.tx" x="0" y="32" rotation="90"/>
+  </objectgroup>
+ </tile>
+ <tile id="166">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-diagonal.tx" x="32" y="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="183">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="184">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="185">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="186">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="187">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-diagonal.tx" x="0" y="32" rotation="90"/>
+  </objectgroup>
+ </tile>
+ <tile id="188">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-diagonal.tx" x="32" y="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="189">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="192">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="193">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-diagonal.tx" x="0" y="32" rotation="90"/>
+  </objectgroup>
+ </tile>
+ <tile id="194">
+  <objectgroup draworder="index">
+   <object id="1" template="templates/collision-diagonal.tx" x="32" y="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="195">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="212">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="213">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="214">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="215">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="216">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="217">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="218">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="221">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="222">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="223">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="224">
+  <objectgroup draworder="index">
+   <object id="1" template="full-collision.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
 </tileset>
