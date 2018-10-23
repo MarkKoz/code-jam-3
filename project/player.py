54e21266372dd522db89cdd380a82d56b7cbe1d0
MarkKoz <KozlovMark@gmail.com>
2018-10-22 23:57:07 -0700

    Add player class

---
+++ project/player.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+        self.image = pygame.Surface((32, 48))
+        self.image.fill(pygame.Color('yellow'))
+        self.rect = self.image.get_rect()
+        self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 8)
+
+        self.velocity = [0, 0]
+        self.position = [0, 0]
+
+    def update(self, time_delta):
+        self.position[0] += self.velocity[0] * time_delta
+        self.position[1] += self.velocity[1] * time_delta
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
