6539104c4429a8dc21b68117382331b34a96ca63
MarkKoz <KozlovMark@gmail.com>
2018-10-22 17:19:43 -0700

    Add map and textures

---
+++ assets/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="Background" tilewidth="64" tileheight="64" tilecount="56" columns="14">
+ <image source="textures/Background.png" width="950" height="256"/>
+</tileset>
---
+++ assets/Main.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="Main" tilewidth="32" tileheight="32" tilecount="232" columns="29">
+ <image source="textures/tileset.png" width="950" height="256"/>
+</tileset>
