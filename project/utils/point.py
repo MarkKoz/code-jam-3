5c4a58647efbe13f2f081431fdedba617f99cb50
MarkKoz <KozlovMark@gmail.com>
2018-10-25 22:24:55 -0700

    Add unpacking support to Dimensions and Point

--- project/utils/point.py
+++ project/utils/point.py
@@ -6,8 +6,14 @@ class Point:
     x: float = attr.ib()
     y: float = attr.ib()
 
+    def __iter__(self):
+        return (getattr(self, f.name) for f in attr.fields(self.__class__))
+
 
 @attr.s(slots=True, auto_attribs=True)
 class Dimensions:
     width: int = attr.ib()
     height: int = attr.ib()
+
+    def __iter__(self):
+        return (getattr(self, f.name) for f in attr.fields(self.__class__))
