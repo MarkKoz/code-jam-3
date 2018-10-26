bb0047b6e94cf3d51fdda61eec2f81cc4c75ff29
MarkKoz <KozlovMark@gmail.com>
2018-10-25 19:51:11 -0700

    Implement input component for Player

--- project/entities/player/input.py
+++ project/entities/player/input.py
@@ -1,5 +1,14 @@
+import pygame
+
 from .player_entity import Player
 from components import InputComponent
+from utils import Direction
+
+ACCELERATION = 300
+CONTROLS = {
+    Direction.LEFT: {pygame.K_LEFT, pygame.K_a},
+    Direction.RIGHT: {pygame.K_RIGHT, pygame.K_d}
+}
 
 
 class PlayerInputComponent(InputComponent):
@@ -8,3 +17,22 @@ class PlayerInputComponent(InputComponent):
             key, up = self._get_input_key()
         except TypeError:
             return
+
+        if key in CONTROLS[Direction.LEFT]:
+            self._move(player, Direction.LEFT, up)
+        elif key in CONTROLS[Direction.RIGHT]:
+            self._move(player, Direction.RIGHT, up)
+
+    @staticmethod
+    def _move(player: Player, direction: Direction, up: bool):
+        current_keys = set(pygame.key.get_pressed())
+
+        if up:
+            if CONTROLS[Direction(-direction)] & current_keys:
+                player.velocity.x = -ACCELERATION
+                player.orientation = -direction
+            else:
+                player.velocity.x = 0
+        else:
+            player.velocity.x = -ACCELERATION
+            player.orientation = direction
