17b907986c999c03971fd3892142815a01e1ff47
MarkKoz <KozlovMark@gmail.com>
2018-10-25 22:01:41 -0700

    Add World class
    The class handles loading of the map and its collisions data

---
+++ project/core/world.py
@@ -0,0 +1,31 @@
+from typing import Tuple
+
+import pygame
+import pyscroll
+import pytmx
+from pytmx.util_pygame import load_pygame
+
+from utils import Triangle
+
+
+class World:
+    def __init__(self, file_path):
+        tmx_data = load_pygame(file_path)  # Load data from pytmx
+        self.map_data = pyscroll.data.TiledMapData(tmx_data)  # Create new data source for pyscroll
+        self.rects, self.slopes = self._get_collisions(tmx_data)
+
+    @staticmethod
+    def _get_collisions(tmx_data: pytmx.TiledMap) -> Tuple:
+        rects = []
+        slopes = []
+
+        obj: pytmx.TiledObject
+        for obj in tmx_data.objects:
+            points = getattr(obj, 'points', None)
+            if points:
+                slopes.append(Triangle(obj))
+            else:
+                rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
+                rects.append(rect)
+
+        return tuple(rects), tuple(slopes)
