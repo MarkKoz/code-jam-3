7db23d4937225cb5e1c650ded6f8749950942c37
MarkKoz <KozlovMark@gmail.com>
2018-10-25 17:03:22 -0700

    Player entity subclasses Sprite

--- project/entities/player/player_entity.py
+++ project/entities/player/player_entity.py
@@ -1,8 +1,10 @@
+from pygame.sprite import Sprite
+
 from components import GraphicsComponent, InputComponent, PhysicsComponent
 from entities.entity import Entity
 
 
-class Player(Entity):
+class Player(Entity, Sprite):
     def __init__(self, graphics: GraphicsComponent, _input: InputComponent, physics: PhysicsComponent, **kwargs):
         super().__init__(**kwargs)
 
