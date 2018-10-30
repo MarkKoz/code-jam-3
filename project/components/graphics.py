562fa483cd2245837bc57c1a15bc0b0c47a1f78a
MarkKoz <KozlovMark@gmail.com>
2018-10-30 05:16:00 -0700

    Add helper method for loading sets of textures

--- project/components/graphics.py
+++ project/components/graphics.py
@@ -1,5 +1,14 @@
+import pygame
+
 from .component import Component
 
 
 class GraphicsComponent(Component):
-    pass
+    @staticmethod
+    def load_textures(path: str, count: int, alpha: bool = True):
+        for i in range(count):
+            image: pygame.Surface = pygame.image.load(path.format(i))
+            if alpha:
+                image = image.convert_alpha()
+
+            yield image
