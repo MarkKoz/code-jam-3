d4f9db252b6f9739cae6bb6f707ac0b600dcb27c
MarkKoz <KozlovMark@gmail.com>
2018-10-30 23:15:21 -0700

    Add fonts

--- project/core/constants.py
+++ project/core/constants.py
@@ -7,6 +7,11 @@ MAP_PATH = 'assets/map.tmx'
 JUICE_FILL_RATE = 5
 SCREEN_SCALE = 2
 
+FONTS = {
+    'too much ink': 'assets/fonts/TooMuchInk.ttf',
+    'monogram': 'assets/fonts/monogram_extended.ttf'
+}
+
 CONTROLS_HELP_TEXT = b"""\
 Forward: D or \xe2\x86\x92
 Back: A or \xe2\x86\x90
