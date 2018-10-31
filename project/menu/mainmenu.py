d6d441ed0954a752a20ff28a9800c45bff185a7c
scragly <scragly@gmail.com>
2018-10-31 22:02:10 +1000

    Fix menu display bug

--- project/menu/mainmenu.py
+++ project/menu/mainmenu.py
@@ -22,6 +22,7 @@ class MainMenu:
         self._controls_info_rect: pygame.Rect = self._controls_info.get_rect()
 
         self._set_screen(width, height)
+        self.resize(width, height)
 
     def _set_screen(self, width, height):
         self.screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
